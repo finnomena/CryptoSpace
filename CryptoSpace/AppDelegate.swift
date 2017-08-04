a0338e255bf78b0a364053506694f850154ebac9
Pitchmak <pitchmak@gmail.com>
2017-08-04 14:49:42 +0700

    Initial Commit

---
+++ CryptoSpace/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CryptoSpace
+//
+//  Created by Pitchmak Sareerat on 8/4/2560 .
+//  Copyright © 2560 Finnomena. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CryptoSpace/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CryptoSpace
+//
+//  Created by Pitchmak Sareerat on 8/4/2560 .
+//  Copyright © 2560 Finnomena. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
